5c4bd6a2031490c495db482213b070b06d14ddfb
JunZhou <478103223@qq.com>
2025-07-31 18:32:56 +0800

    chore:jenkins 打包优化

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -2,84 +2,245 @@ pipeline {
     agent any
     environment {
         //项目名称
-        PROJECT_NAME = "PipelineTEST"
+        PROJECT_NAME = 'PipelineTEST'
         //Unity 编辑器路径
-        UNITY_EDITOR_PATH = "C:\\Program Files\\Unity\\Hub/Editor\\2021.3.20f1\\Editor\\Unity.exe"
+        // UNITY_EDITOR_PATH = "C:\\Program Files\\Unity\\Hub\\Editor\\2021.3.20f1\\Editor\\Unity.exe"
+        UNITY_EDITOR_PATH = 'C:\\Unity\\UnityEditor\\Unity 2020.3.33f1\\Editor\\Unity.exe'
         //Unity 项目路径
-        UNITY_PROJECT_PATH = "C:/MyGit/AutoBuildUnity"
+        UNITY_PROJECT_PATH = 'C:\\MyGit\\AutoBuildUnity'
         //Android 项目路径
-        ANDROID_PROJECT_PATH = "C:/MyGit/AutoBuildUnity_Build"
+        ANDROID_PROJECT_PATH = 'C:\\MyGit\\AutoBuildUnity_Build'
+        //UnityLog Path
+        UNITY_LOG_PATH = "C:\\IIS_ServerData\\${PROJECT_NAME}\\UnityLog\\${VERSION_NAME}.log"
+        //包体输出目录
+        BUILD_OUTPUT_PATH = "C:\\IIS_ServerData\\${PROJECT_NAME}\\BuildOutput\\${VERSION_NAME}"
     }
     stages {
-        stage('Log') {
+        stage('Log And Init') {
             steps {
-                bat '''
-                    @echo %PROJECT_NAME% 
-                    @echo %UNITY_EDITOR_PATH% 
-                    @echo %UNITY_PROJECT_PATH% 
-                    @echo %ANDROID_PROJECT_PATH% 
-                    @echo %VERSION_NAME% 
-                '''
+                script {
+                    bat '''
+                        @echo %PROJECT_NAME%
+                        @echo %UNITY_EDITOR_PATH%
+                        @echo %UNITY_PROJECT_PATH%
+                        @echo %ANDROID_PROJECT_PATH%
+                        @echo %UNITY_LOG_PATH%
+                        @echo %BUILD_OUTPUT_PATH%
+                    '''
+
+                      // 获取当前时间并格式化
+                    def currentTime = new Date().format("yyyy-MM-dd_HH-mm-ss")
+                    echo "当前时间: ${currentTime}"
+                    
+                    // 将当前时间传递给后续的 bat 脚本
+                    env.CURRENT_TIME = currentTime
+                }
             }
         }
         stage('Unity Git Sync') {
+            when { expression { SYNC_UNITY_GIT == 'true' } }
             steps {
                 timeout(time: 1, unit: 'MINUTES') {
                     script {
-                       bat 'cd %UNITY_PROJECT_PATH% && git checkout -- .'
-                       bat 'cd %UNITY_PROJECT_PATH% && git pull'
+                        bat 'cd %UNITY_PROJECT_PATH% && git checkout -- .'
+                        bat 'cd %UNITY_PROJECT_PATH% && git pull'
                     }
                 }
             }
         }
-        stage("Kill Unity"){
-             when{expression {BUILD_UNITY == "true"}}
-             steps{
-                 script{
-                      bat '''
+        stage('Kill Unity') {
+            when { expression { BUILD_UNITY == 'true' } }
+            steps {
+                script {
+                    bat '''
                         REM 判断Unity是否运行中
                         TASKLIST /V /S localhost /U %username% > tmp_process_list.txt
                         TYPE tmp_process_list.txt | FIND "Unity.exe"
-                        
+
                         IF ERRORLEVEL 0 (
                             GOTO UNITY_IS_RUNNING
                         ) ELSE (
                             GOTO START_UNITY
                         )
-                        
+
                         :UNITY_IS_RUNNING
                         :: 杀掉Unity
                         TASKKILL /F /IM Unity.exe
-                        
+
                         :: 停1秒
                         PING 127.0.0.1 -n 3 >NUL
-                        
+
                         GOTO END
-                        
+
                         :END
                         REM 删除临时文件
                         del tmp_process_list.txt
                     '''
-                 }
-             }
+                }
+            }
         }
-        stage("Build Unity") {
-           when { expression { BUILD_UNITY == "true" } }
-           steps {
-               bat '''
-               "C:\\Unity\\UnityEditor\\Unity 2020.3.33f1\\Editor\\Unity.exe" -keep -batchmode -projectPath C:\\MyGit\\AutoBuildUnity -executeMethod BuildProject.TestBuildSuccess -logFile C:\\IIS_ServerData\\PipelineTEST\\UnityLog\\100.log --productName:Idle_Lose_Weight --version:1.0.0 -buildTarget:Android -customParam:1
-               '''
-           }
+        stage('Build Unity') {
+            when { expression { BUILD_UNITY == 'true' } }
+            steps {
+                timeout(time: 60, unit: 'MINUTES') {
+                    bat '''
+                   "%UNITY_EDITOR_PATH%" -keep -batchmode -projectPath %UNITY_PROJECT_PATH% -executeMethod BuildProject.TestBuildSuccess -logFile %UNITY_LOG_PATH% --productName:%PROJECT_NAME% --version:%VERSION_CODE% -buildTarget:Android -customParam:%UNITY_CUSTOME_PARAM%
+                   '''
+                }
+            }
         }
         stage('Android Git Sync') {
+            when { expression { SYNC_ANDDROID_GIT == 'true' } }
             steps {
-                timeout(time: 1, unit: 'MINUTES') {
+                timeout(time: 30, unit: 'MINUTES') {
                     script {
-                       bat 'cd %ANDROID_PROJECT_PATH% && git checkout -- .'
-                       bat 'cd %UNITY_PROJECT_PATH% && git pull'
+                        bat 'cd %ANDROID_PROJECT_PATH% && git checkout -- .'
+                        bat 'cd %UNITY_PROJECT_PATH% && git pull'
                     }
                 }
             }
-        }   
+        }
+        stage('Clean Android') {
+            when { expression { CLEAN_ANDROID_CACHED == 'true' } }
+            steps {
+                timeout(time: 60, unit: 'MINUTES') {
+                    bat '''
+                    cd %ANDROID_PROJECT_PATH%
+                    gradlew.bat clean
+                   '''
+                }
+            }
+        }
+        stage('Sync Android') {
+            steps {
+                timeout(time: 60, unit: 'MINUTES') {
+                    bat '''
+                    cd %ANDROID_PROJECT_PATH%
+                    gradlew syncReleaseLibJars
+                   '''
+                }
+            }
+        }
+        stage('Build Release APK') {
+            when { expression { BUILD_ANDROID_APK == 'true' } }
+            steps {
+                timeout(time: 60, unit: 'MINUTES') {
+                    script {
+                        bat '''
+                            cd %ANDROID_PROJECT_PATH%
+                            gradlew.bat assembleRelease  --stacktrace
+                            '''
+                        // bat '''
+                        //     cd %ANDROID_PROJECT_PATH%
+                        //     apksigner sign --ks %pstoreFilefile%  --ks-pass pass:%storePassword%  --ks-key-alias %keyAlias% --key-pass pass:%keyPassword%
+                        //     '''
+                        bat '''
+                            @echo off
+                            set "source=%ANDROID_PROJECT_PATH%\\launcher\\build\\outputs\\apk\\release\\launcher-release-unsigned.apk"
+                            set "dest=%BUILD_OUTPUT_PATH%\\%PROJECT_NAME%_%VERSION_NAME%_Release_%CURRENT_TIME%.apk"
+                        
+                            :: 创建目标目录（自动创建父目录）
+                            if not exist "%dest%\\.." mkdir "%dest%\\.." 2>&1 || (
+                                echo Failed to create target directory
+                                exit /b 1
+                            )
+                        
+                            :: 检查源文件存在
+                            if not exist "%source%" (
+                                echo Source file not found: %source%
+                                exit /b 2
+                            )
+                        
+                            :: 执行拷贝
+                            copy /y "%source%" "%dest%" 2>&1 || (
+                                echo Copy failed
+                                exit /b 3
+                            )
+                        '''
+                    }
+                }
+            }
+        }
+        stage('Build Release AAB') {
+            when { expression { BUILD_ANDROID_AAB == 'true' } }
+            steps {
+                timeout(time: 60, unit: 'MINUTES') {
+                    script {
+                        bat '''
+                            cd %ANDROID_PROJECT_PATH%
+                            gradlew.bat bundleRelease  --stacktrace
+                            '''
+                        // bat '''
+                        //     cd %ANDROID_PROJECT_PATH%
+                        //     apksigner sign --ks %pstoreFilefile%  --ks-pass pass:%storePassword%  --ks-key-alias %keyAlias% --key-pass pass:%keyPassword%
+                        //     '''
+                        bat '''
+                            @echo off
+                            set "source=%ANDROID_PROJECT_PATH%\\launcher\\build\\outputs\\apk\\release\\launcher-release-unsigned.aab"
+                            set "dest=%BUILD_OUTPUT_PATH%\\%PROJECT_NAME%_%VERSION_NAME%_Release_%CURRENT_TIME%.aab"
+                        
+                            :: 创建目标目录（自动创建父目录）
+                            if not exist "%dest%\\.." mkdir "%dest%\\.." 2>&1 || (
+                                echo Failed to create target directory
+                                exit /b 1
+                            )
+                        
+                            :: 检查源文件存在
+                            if not exist "%source%" (
+                                echo Source file not found: %source%
+                                exit /b 2
+                            )
+                        
+                            :: 执行拷贝
+                            copy /y "%source%" "%dest%" 2>&1 || (
+                                echo Copy failed
+                                exit /b 3
+                            )
+                        '''
+                    }
+                }
+            }
+        }
+        stage('Build Debug APK') {
+            when { expression { ONLY_RELEASE == 'false' } }
+            steps {
+                timeout(time: 60, unit: 'MINUTES') {
+                    script {
+                        bat '''
+                            cd %ANDROID_PROJECT_PATH%
+                            gradlew.bat assembleDebug --stacktrace
+                            '''
+                        // bat '''
+                        //     cd %ANDROID_PROJECT_PATH%
+                        //     apksigner sign --ks %pstoreFilefile%  --ks-pass pass:%storePassword%  --ks-key-alias %keyAlias% --key-pass pass:%keyPassword%
+                        //     '''
+                        bat '''
+                            @echo off
+                            set "source=%ANDROID_PROJECT_PATH%\\launcher\\build\\outputs\\apk\\Debug\\launcher-Debug-unsigned.apk"
+                            set "dest=%BUILD_OUTPUT_PATH%\\%PROJECT_NAME%_%VERSION_NAME%_Debug_%CURRENT_TIME%.apk"
+                        
+                            :: 创建目标目录（自动创建父目录）
+                            if not exist "%dest%\\.." mkdir "%dest%\\.." 2>&1 || (
+                                echo Failed to create target directory
+                                exit /b 1
+                            )
+                        
+                            :: 检查源文件存在
+                            if not exist "%source%" (
+                                echo Source file not found: %source%
+                                exit /b 2
+                            )
+                        
+                            :: 执行拷贝
+                            copy /y "%source%" "%dest%" 2>&1 || (
+                                echo Copy failed
+                                exit /b 3
+                            )
+                        '''
+                    }
+                }
+            }
+        }
     }
 }
+
